f9bd1cd5c82b154ee7a9772319531e1e1200f28c
lai2-24 <lai2-24@itismeucci.com>
2024-04-15 09:06:35 +0200

    commit finale

---
+++ php/index.php
@@ -0,0 +1,14 @@
+<?php
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/controllers/AlunniController.php';
+
+$app = AppFactory::create();
+
+$app->get('/alunni', "AlunniController:index");
+$app->get('/alunni/{id}', "AlunniController:getAlunno");
+$app->post('/alunni', "AlunniController:postAlunni");
+$app->put('/alunni/{id}', "AlunniController:putAlunno");
+$app->delete('/alunni/{id}', "AlunniController:deleteAlunno");
+$app->run();
